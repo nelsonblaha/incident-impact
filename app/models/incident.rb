4f39948973f4217a3ea0f6d013990186d4c3c2e0
rlavery <robertl@uta.edu>
2013-11-15 13:50:50 -0600

    Time impact functionality complete.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -5,9 +5,21 @@ class Incident < ActiveRecord::Base
   def calculate_impact
     self.impact_index = self.initial_service_rating +
                         self.service_impact
-    # Time impact and escalation policy are currently added 
-    # to the index conditionally until I complete implementation.
-    
+        
+    if Time.now.saturday? || Time.now.sunday?
+      self.time_impact = 2 if Time.now.hour > 9 && Time.now.hour < 18
+    elsif Time.now.hour > 7 && Time.now.hour < 21
+      self.time_impact = 3
+    else
+      self.time_impact = 1
+    end
+
+    ACADEMIC_CALENDAR_HOLIDAYS.each do |time|
+      self.time_impact = 0 if Time.now.to_date == Time.utc(*time).to_date
+    end
+    ACADEMIC_CALENDAR_FINALS.each do |time|
+      self.time_impact = 4 if Time.now.to_date == Time.utc(*time).to_date
+    end
     self.impact_index += self.time_impact if self.time_impact
     self.impact_index += self.escalation_policy if self.escalation_policy
                         
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -21,3 +20,4 @@ module IncidentImpactTracker
     # config.i18n.default_locale = :de
   end
 end
+
